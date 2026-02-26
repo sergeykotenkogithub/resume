217c518ec692ded56a063f60cbede184a3b1179c
Сергей Котенко <sergeykotenko@bk.ru>
2026-02-27 01:10:12 +0300

    Fix avatar rotation

--- src/widgets/hero/ui/Hero.tsx
+++ src/widgets/hero/ui/Hero.tsx
@@ -147,10 +147,12 @@ const AvatarImage = styled.img`
   width: 100%;
   height: 100%;
   object-fit: cover;
+  object-position: center;
   position: absolute;
   top: 0;
   left: 0;
   z-index: 1;
+  transform: rotate(-5deg);
 `;
 
 const FloatingBadge = styled.div`
